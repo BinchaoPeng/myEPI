25a9f55598ebf15cdd159aaaed9b0740fdf56e54
ubuntupbc <646047990@qq.com>
2021-03-16 16:36:58 +0800

    3

---
+++ src/model/model-transformer.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+embedding_matrix = torch.as_tensor(np.load("../embedding_matrix.npy"))
+MAX_LEN_en = 3000  # seq_lens
+MAX_LEN_pr = 2000  # seq_lens
+NB_WORDS = 4097  # one-hot dim
+EMBEDDING_DIM = 100
